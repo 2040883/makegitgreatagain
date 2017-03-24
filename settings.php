1f14da99f239f219bedd57fea20e315bc8745f61
2040883 <2040883@student.swin.edu.au>
2017-03-24 16:12:22 +1100

    mysql connect info

---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php 
+ $host = "13.54.41.3:3306"; 
+ $user = "root"; // your user name 
+ $pwd = "jazon@12"; // your password (date of birth ddmmyy unless changed) 
+ $sql_db = "pharmacy"; // your database 
+?>
